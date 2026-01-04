1f4573388ae6358beaa1b3c247a21ee8d1349e5e
eugene fidelis <eugenefidelis573@gmail.com>
2026-01-04 23:09:10 +0100

    hero

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Hero from "./components/landing/Hero";
-import About from "./components/landing/About";
 export default function page() {
   return (
     <>
       <Hero />
-      <About />
     </>
   );
 }
